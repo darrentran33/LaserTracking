3673c120533155314315879df167179a47eefc83
Darren Tran <66705232+darrentran33@users.noreply.github.com>
2020-06-17 16:54:35 -0400

    Update VideoAnalysis.py
    altered the code to move in 1 DOF will work on setting it so it responds to another degree of freedom

--- Scripts/VideoAnalysis.py
+++ Scripts/VideoAnalysis.py
@@ -1,12 +1,27 @@
 import numpy as np
 import cv2
+from PCA9685 import PCA9685
+
+pwm = = PCA9685(0x40, debug = False)
+pwm.setPWMFreq(60)
+pwm.setServoPosition(0,90)
+rows, cols, _ = frame.shape 
+
 
 cap = cv2.VideoCapture(0)
+
+cap.set(3,480)
+cap.set(4,320)
+
+center = int(cols/2)
+moving_center = int(cols/2)
+
+angle = 90
 
 ret, frame1 = cap.read()
 ret, frame2 = cap.read()
 print(frame1.shape)
-while cap.isOpened():
+while True:
     diff = cv2.absdiff(frame1, frame2)
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5,5), 0)
@@ -25,13 +40,23 @@ while cap.isOpened():
         cX = int(M["m10"] / M["m00"])
         cY = int(M["m01"] / M["m00"])
         
+        moving_center = int((x + x + w)/2)
+        
+        break
+        
         print("X-coord: {}, Y-coord: {}" .format(cX,cY))
        
-    image = cv2.resize(frame1, (1280,720))
+    #image = cv2.resize(frame1, (1280,720))
     out.write(image)
     cv2.imshow("feed", frame1)
     frame1 = frame2
     ret, frame2 = cap.read()
+    
+    if moving_center < center
+        position += 1.5
+    if moving_center > center
+        position -= 1.5
+    pwm.setServoPosition(0,0,position)
 
     ch = cv2.waitKey(1)
     if ch & 0xFF == ord('q'):
